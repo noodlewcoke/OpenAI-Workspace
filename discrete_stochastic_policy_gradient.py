8f2bc6b03792a004158fde9544f4f04b6db2fe24
Ismagil Uzdenov <isouzd@gmail.com>
2017-04-21 15:45:51 +0300

    Update 1: dspg with proper loss and cem

--- discrete_stochastic_policy_gradient.py
+++ discrete_stochastic_policy_gradient.py
@@ -2,38 +2,17 @@ import tensorflow as tf
 import numpy as np
 import random
 
-def scramble(orig):
-    scrambled = orig[:]
-    random.shuffle(scrambled)
-    return scrambled
-
-# REPLAY BUFFER -> (s, a, s', r, d)
-def discount(replay_buffer, gamma):
-    disrb = replay_buffer[:]
-    for i,rb in enumerate(disrb):
-        for j,drb in enumerate(disrb[(i):]):
-            rb[3] += drb[3]*gamma**(j) if not j==0 else 0
-            if drb[4]:
-                break
-    return disrb
-
-def v0_reward(replay_buffer):
-    disrb = replay_buffer[:]
-    for i,rb in enumerate(disrb):
-        r0 = rb[3]
-        for j,drb in enumerate(disrb[(i):]):
-            rb[3] += drb[3] if not j==0 else 0
-            drb[3] += r0 if j==1 else 0
-            if drb[4]:
-                break
-    return disrb
-
 def make_np_array(orig):
     npar = np.zeros((len(orig),4))
     for i,obj in enumerate(orig):
         npar[i] = np.array(obj, dtype=np.float32)
     return npar
 
+def index_mat(indices, batch_size, exitus):
+    dummy = np.zeros((batch_size,exitus), dtype=np.int32)
+    for i in range(batch_size):
+        dummy[i] = np.array([i, indices[i]])
+    return dummy
 
 class discrete_spg():
     def __init__(self, initus, exitus):
@@ -43,29 +22,35 @@ class discrete_spg():
         self.net_in = tf.placeholder(tf.float32, [None, initus], name="Network_Input") #network input
         self.lr = tf.placeholder(tf.float32) #learning rate
         self.disreward = tf.placeholder(tf.float32, [None]) #discounted reward
-        self.sampled_action = tf.placeholder(tf.int32, [None])
+        self.sampled_action = tf.placeholder(tf.int32, [None,2*exitus])
+        self.sampled_action_ce = tf.placeholder(tf.int32, [None])
 
         weight_init = tf.contrib.layers.xavier_initializer()
 
-        fc_1 = tf.contrib.layers.fully_connected(inputs=self.net_in, num_outputs=50, activation_fn=tf.nn.relu, weights_initializer=weight_init)
-        fc_2 = tf.contrib.layers.fully_connected(inputs=fc_1, num_outputs=30, activation_fn=tf.nn.relu, weights_initializer=weight_init)
+        fc_1 = tf.contrib.layers.fully_connected(inputs=self.net_in, num_outputs=32, activation_fn=tf.nn.relu, weights_initializer=weight_init)
+        fc_2 = tf.contrib.layers.fully_connected(inputs=fc_1, num_outputs=32, activation_fn=tf.nn.relu, weights_initializer=weight_init)
+
+        # #"manual" implementation
+        # self.exit = tf.contrib.layers.fully_connected(inputs=fc_2, num_outputs=2*self.exitus, activation_fn=tf.nn.softmax, weights_initializer=weight_init)
+        # self.loss  = -self.disreward*tf.log(tf.gather_nd(self.exit, self.sampled_action))
+
+        #softmax cross-entropy method
         self.exit = tf.contrib.layers.fully_connected(inputs=fc_2, num_outputs=2*self.exitus, activation_fn=None, weights_initializer=weight_init)
+        self.loss = -self.disreward*tf.log(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.exit, labels=self.sampled_action_ce))
+
 
         self.action = tf.argmax(self.exit, axis=1)
-
-        self.loss = -self.disreward*tf.log(self.exit[-1,self.sampled_action[-1]]+1e-06)
-        #self.loss = self.disreward*tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.exit, labels=self.sampled_action)
         self.opt = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
 
     def run(self, session, state):
         return session.run([self.exit,self.action], feed_dict={self.net_in:state})
 
     def train(self, session, replay_buffer, batch_size, learning_rate):
-        rpb = discount(replay_buffer, 0.99)
-        #rpb = v0_reward(replay_buffer)
-        result=[]
-        drpb = np.array(scramble(rpb)[:batch_size])
-        #for rb in drpb[:batch_size]:
-        #session.run([self.loss,self.opt], feed_dict={self.net_in: rb[0], self.sampled_action:rb[1], self.disreward:rb[3], self.lr:learning_rate})[0]
-        session.run([self.loss,self.opt], feed_dict={self.net_in: make_np_array(drpb[:,0]), self.sampled_action:drpb[:,1], self.disreward:drpb[:,3], self.lr:learning_rate})[0]
-        #print min(result)
+        drpb = np.array(replay_buffer[:batch_size])
+
+        # #"manual" implementation
+        # sam_act = index_mat(drpb[:,1], batch_size, 2*self.exitus)
+        # session.run([self.loss,self.opt], feed_dict={self.net_in: make_np_array(drpb[:,0]), self.sampled_action:sam_act, self.disreward:drpb[:,3], self.lr:learning_rate})[0]
+
+        #softmax cross-entropy method
+        session.run([self.loss,self.opt], feed_dict={self.net_in: make_np_array(drpb[:,0]), self.sampled_action_ce:drpb[:,1], self.disreward:drpb[:,3], self.lr:learning_rate})[0]
--- trial_pit.py
+++ trial_pit.py
@@ -3,12 +3,39 @@ import tensorflow as tf
 import numpy as np
 import continuous_stochastic_policy_gradient as cspg
 import discrete_stochastic_policy_gradient as dspg
+import random
 
 
 total_timesteps = 400
 episode_number = 10000
 max_timestep = 300
-batch_size = 64
+batch_size = 1000
+
+def scramble(orig):
+    scrambled = orig[:]
+    random.shuffle(scrambled)
+    return scrambled
+
+# REPLAY BUFFER -> (s, a, s', r, d)
+def discount(replay_buffer, gamma):
+    disrb = replay_buffer[:]
+    for i,rb in enumerate(disrb):
+        for j,drb in enumerate(disrb[(i):]):
+            rb[3] += drb[3]*gamma**(j) if not j==0 else 0
+            if drb[4]:
+                break
+    return disrb
+
+def v0_reward(replay_buffer):
+    disrb = replay_buffer[:]
+    for i,rb in enumerate(disrb):
+        r0 = rb[3]
+        for j,drb in enumerate(disrb[(i):]):
+            rb[3] += drb[3] if not j==0 else 0
+            drb[3] += r0 if j==1 else 0
+            if drb[4]:
+                break
+    return disrb
 
 def discrete_spg_cartpole():
     env = gym.make('CartPole-v0')
@@ -17,6 +44,8 @@ def discrete_spg_cartpole():
     spg = dspg.discrete_spg(4, 1)
     sess.run(tf.global_variables_initializer())
     replay_buffer = []
+    mini_buffer = []
+    trained = 0
     for episode in xrange(episode_number):
         observation = env.reset()
         R = 0
@@ -24,19 +53,24 @@ def discrete_spg_cartpole():
         for t in xrange(total_timesteps):
             #env.render()
             if len(replay_buffer)>=batch_size:
-                spg.train(sess, replay_buffer, batch_size, 0.00001)
+                main_buffer = scramble(replay_buffer)
+                spg.train(sess, main_buffer, batch_size, 0.0001)
+                trained += 1
+                #print trained
             prob, action = spg.run(sess, np.expand_dims(observation, 0))
             old_obs = observation
             #print prob
             observation, reward, done, info = env.step(np.squeeze(action))
             R += reward
             flag +=1
-            replay_buffer.append([np.expand_dims(old_obs, 0), np.squeeze(action), np.expand_dims(observation, 0), reward, done])
+            mini_buffer.append([np.expand_dims(old_obs, 0), np.squeeze(action), np.expand_dims(observation, 0), reward, done])
 
             if flag==max_timestep:# or t==total_timesteps-1:
                 done = 1
             if done:
-                print episode, R
+                replay_buffer += discount(mini_buffer, 0.99)
+                mini_buffer = []
+                print episode, R, len(replay_buffer)
                 break
         #spg.train(sess, main_buffer, 0.001)
     sess.close()
